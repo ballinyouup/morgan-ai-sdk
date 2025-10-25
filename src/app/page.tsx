93d23d78ac03e9be309b57dac8420e75f0f93e9e
ballinyouup <bryanhuici@gmail.com>
2025-10-25 18:00:50 -0400

    implemented design from v0 and deleted old design

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatInterface } from "@/components/chat-interface"
-
-export default function Home() {
-    return (
-        <main className="h-screen w-full">
-            <ChatInterface />
-        </main>
-    )
-}
